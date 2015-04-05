684c544cfe998dbe5c40294c0284bf7e89f13142
Ed Prokhor <eprokhor@uw.edu>
2015-04-04 22:15:12 -0700

    Started to work on my screen, Forgot pass.

---
+++ app/src/main/java/uw/buuteeq_ponyhax/app/ForgotActivity.java
@@ -0,0 +1,16 @@
+package uw.buuteeq_ponyhax.app;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by eduard_prokhor on 4/4/15.
+ */
+public class ForgotActivity extends ActionBarActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
