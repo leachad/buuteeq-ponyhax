e17d8d67973695b905955889be1f0829bf0fda54
Ed Prokhor <eprokhor@uw.edu>
2015-04-08 17:08:14 -0700

    working on backend for forgot.

--- app/src/main/java/uw/buuteeq_ponyhax/app/ForgotActivity.java
+++ app/src/main/java/uw/buuteeq_ponyhax/app/ForgotActivity.java
@@ -2,6 +2,11 @@ package uw.buuteeq_ponyhax.app;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by eduard_prokhor on 4/4/15.
@@ -11,17 +16,32 @@ public class ForgotActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.forgot_page);
+        checkUser();
 
     }
 
-
-    public void findUser() {
+    public  void checkUser(){
         UserStorageDatabaseHelper dbHelper = new UserStorageDatabaseHelper(getApplicationContext());
-        UserStorageDatabaseHelper.UserCursor cursor = dbHelper.queryUsers();
+        final UserStorageDatabaseHelper.UserCursor cursor = dbHelper.queryUsers();
 
-        while(cursor.moveToNext()){
-            cursor.
-        }
+        final EditText userinput = (EditText) findViewById(R.id.userForPasswordreset);
+
+        userinput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    if (!userinput.getText().toString().trim().matches(""))
+                        while (cursor.moveToNext()) {
+                            User temp = cursor.getUser();
+
+                            if (temp.getUserName().trim().matches(userinput.getText().toString().trim())) {
+                                ((TextView) findViewById(R.id.resetSecurityQuestion)).setText(cursor.getSecurityQuestion());
+                            }
+                        }
+                }
+                return false;
+            }
+        });
     }
-
 }
+
