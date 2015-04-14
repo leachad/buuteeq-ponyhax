a16fd73df77c27746d35c9a5908b7908773936d8
Ed Prokhor <eprokhor@uw.edu>
2015-04-13 23:41:43 -0700

    man people messing with my file

--- app/src/main/java/uw/buuteeq_ponyhax/app/SettingsFragment.java
+++ app/src/main/java/uw/buuteeq_ponyhax/app/SettingsFragment.java
@@ -5,10 +5,19 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.Spinner;
+<<<<<<< Updated upstream
+=======
+import android.widget.TextView;
+import android.widget.Toast;
+>>>>>>> Stashed changes
 
 import db.User;
 
@@ -36,15 +45,15 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        SharedPreferences prefs = getActivity().getSharedPreferences(User.USER_PREFS, Context.MODE_PRIVATE);
+        final SharedPreferences prefs = getActivity().getSharedPreferences(User.USER_PREFS, Context.MODE_PRIVATE);
         /** Instantiante the security question spinner.*/
         mQuestionSpinner = (Spinner) getActivity().findViewById(R.id.spinnerSecurityQuestion_reset);
 
-        User myRegisteredUser = new User();
+        final User myRegisteredUser = new User();
         /** String value to update the New User security question field is gleaned from the spinner.*/
         myRegisteredUser.setSecurityQuestion(mQuestionSpinner.getSelectedItem().toString().trim());
 
-        prefs.edit().putString(User.USER_QUESTION, myRegisteredUser.getSecurityQuestion()).apply();
+
 
         getActivity().findViewById(R.id.resetpass_from_settings).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,5 +62,21 @@ public class SettingsFragment extends Fragment {
                 startActivity(myIntent);
             }
         });
+
+        final EditText userinput = (EditText) getActivity().findViewById(R.id.newSecurityAnswer);
+
+        userinput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    prefs.edit().putString(User.USER_QUESTION, myRegisteredUser.getSecurityQuestion()).apply();
+                    prefs.edit().putString(User.USER_ANSWER, userinput.toString()).apply();
+                    Toast.makeText(getActivity().getApplicationContext(),
+                            "Your security Question and answer have been changed.", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+            });
+
     }
 }
