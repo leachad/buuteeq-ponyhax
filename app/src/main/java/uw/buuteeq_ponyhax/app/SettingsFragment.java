4d37d15c6e4caabcddc1240e88d6bfffc13798a2
Andrew Leach <leachad@uw.edu>
2015-04-17 10:10:32 -0700

    Pushed the submit button and all the correct data to the database which will allow the application to kick out a new email to the user when email is back up and functional

--- app/src/main/java/uw/buuteeq_ponyhax/app/SettingsFragment.java
+++ app/src/main/java/uw/buuteeq_ponyhax/app/SettingsFragment.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -21,62 +22,66 @@ import db.UserStorageDatabaseHelper;
 
 /**
  * Created by eduard_prokhor on 4/13/15.
+ * edited by andrew leach on 4/17/15
  */
 public class SettingsFragment extends Fragment {
 
+    /** Fields needed by the whole class.*/
     private Spinner mQuestionSpinner;
+    private EditText mNewSecurityAnswer;
+    private Button mSubmitButton;
+    private SharedPreferences mSharedPreferences;
+    private UserStorageDatabaseHelper mDbHelper;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /** Instantiate the security question spinner.*/
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        View v = inflater.inflate(R.layout.settings_fragment_layout, container, false);
 
-        return v;
+        return inflater.inflate(R.layout.settings_fragment_layout, container, false);
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        final SharedPreferences prefs = getActivity().getSharedPreferences(User.USER_PREFS, Context.MODE_PRIVATE);
-
-        /** Instantiate the security question spinner.*/
         mQuestionSpinner = (Spinner) getActivity().findViewById(R.id.spinnerSecurityQuestion_reset);
-//        final User myRegisteredUser = new User();
-        /** String value to update the New User security question field is gleaned from the spinner.*/
-//        myRegisteredUser.setSecurityQuestion(mQuestionSpinner.getSelectedItem().toString().trim());
+        mNewSecurityAnswer = (EditText) getActivity().findViewById(R.id.securityQuestionAnswer);
+        mSubmitButton = (Button) getActivity().findViewById(R.id.submitNewSecurityAnswer);
+        mDbHelper = new UserStorageDatabaseHelper(getActivity().getApplicationContext());
+        mSharedPreferences = getActivity().getSharedPreferences(User.USER_PREFS, Context.MODE_PRIVATE);
+        mSubmitButton.setOnClickListener(new SubmitListener());
 
         getActivity().findViewById(R.id.resetpass_from_settings).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent myIntent = new Intent(getActivity(), CreateNewPasswordActivity.class);
-                myIntent.putExtra(User.USER_EMAIL, prefs.getString(User.USER_EMAIL, ""));
+                myIntent.putExtra(User.USER_EMAIL, mSharedPreferences.getString(User.USER_EMAIL, ""));
                 startActivity(myIntent);
             }
         });
 
-        final EditText userInput = (EditText) getActivity().findViewById(R.id.newSecurityAnswer);
 
-        userInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                final UserStorageDatabaseHelper dbHelper = new UserStorageDatabaseHelper(getActivity().getApplicationContext());
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    prefs.edit().putString(User.USER_QUESTION, mQuestionSpinner.getSelectedItem().toString().trim()).apply();
-                    prefs.edit().putString(User.USER_ANSWER, userInput.toString().trim()).apply();
-                    Toast.makeText(getActivity().getApplicationContext(), "USER ID IS " + prefs.getLong(User.USER_ID, 0), Toast.LENGTH_SHORT).show();
-                    dbHelper.modifySecurityQuestion(mQuestionSpinner.getSelectedItem().toString().trim(), prefs.getLong(User.USER_ID, 0));
-                    dbHelper.modifySecurityAnswer(userInput.toString(), prefs.getLong(User.USER_ID, 0));
-                    Toast.makeText(getActivity().getApplicationContext(),
-                            "Your security Question and answer have been changed.", Toast.LENGTH_SHORT).show();
-                }
-                return false;
-            }
-        });
 
     }
+
+    private class SubmitListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            mSharedPreferences.edit().putString(User.USER_QUESTION, mQuestionSpinner.getSelectedItem().toString().trim()).apply();
+            mSharedPreferences.edit().putString(User.USER_ANSWER, mNewSecurityAnswer.toString().trim()).apply();
+            Toast.makeText(getActivity().getApplicationContext(), "USER ID IS " + mSharedPreferences.getLong(User.USER_ID, 0), Toast.LENGTH_SHORT).show();
+            mDbHelper.modifySecurityQuestion(mQuestionSpinner.getSelectedItem().toString().trim(), mSharedPreferences.getLong(User.USER_ID, 0));
+            mDbHelper.modifySecurityAnswer(mNewSecurityAnswer.getText().toString(), mSharedPreferences.getLong(User.USER_ID, 0));
+            Toast.makeText(getActivity().getApplicationContext(),
+                    "Your security Question and answer have been changed.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
