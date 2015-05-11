4242cc6fad3e4bce11536746b3b9cf464a26ed48
Andrew <type3dude@gmail.com>
2015-05-10 20:18:41 -0700

    Local storage update

--- app/src/main/java/db/LocalStorage.java
+++ app/src/main/java/db/LocalStorage.java
@@ -1,7 +1,36 @@
 package db;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
- * Created by leachad on 5/10/2015.
+ * Created by leachad on 5/10/2015. Used to statically access and
+ * publish shared preferences.
  */
 public class LocalStorage {
+
+
+    public static void putUserId(String theUserID, Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(User.USER_PREFS, Context.MODE_PRIVATE);
+        sharedPreferences.edit().putString(User.USER_ID, theUserID).apply();
+    }
+
+    public static void putUserEmail(String theEmail, Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(User.USER_PREFS, Context.MODE_PRIVATE);
+        sharedPreferences.edit().putString(User.USER_EMAIL, theEmail).apply();
+    }
+
+    public static void putStartTime(long theStartTime, Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(User.USER_PREFS, Context.MODE_PRIVATE);
+        sharedPreferences.edit().putLong(User.START_TIME, theStartTime).apply();
+    }
+
+    public static void putEndTime(long theEndTime, Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(User.USER_PREFS, Context.MODE_PRIVATE);
+        sharedPreferences.edit().putLong(User.END_TIME, theEndTime).apply();
+    }
+
+    public 
+
 }
--- app/src/main/java/db/User.java
+++ app/src/main/java/db/User.java
@@ -21,6 +21,8 @@ public class User {
     public static final String USER_EMAIL = "userEmail";
     public static final String USER_NAME = "userName";
     public static final String USER_AGREEMENT = "agreement";
+    public static final String START_TIME = "startTime";
+    public static final String END_TIME = "endTime";
 
 
     /**
