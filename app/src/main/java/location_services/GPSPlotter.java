52887e7e09fd47d7d049f8cd81f3c11a41f49f88
Andrew Leach <leachad@uw.edu>
2015-05-20 15:26:53 -0700

    Worked up a quick and dirty class that will issue requests for Locations with Pending Requests.

--- app/src/main/java/location_services/GPSPlotter.java
+++ app/src/main/java/location_services/GPSPlotter.java
@@ -1,10 +1,17 @@
 package location_services;
 
 import android.app.AlarmManager;
+import android.app.IntentService;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.SystemClock;
+
+import android.location.LocationListener;
+import android.util.Log;
 
 /**
  * Created by leachad on 5/20/2015. Will contain
@@ -15,7 +22,16 @@ public class GPSPlotter {
 
     private static final int DEFAULT_INTERVAL = 5;
     private static final int TIMESTAMP_MULTIPLIER = 1000;
+
+
     private static AlarmManager mAlarmManager = null;
+    private static GPSPlotterListener[] networkListeners = null;
+    private static LocationManager mLocationManager = null;
+    //TODO Once network services are up and running, the call will hopefully be
+    //something more like this:
+    //private static GPSPlotterListener[] networkListeners = NetworkServices.getNetworkVariations();
+    //Returns an int or even better an array of listeners, or at the least an array of strings that
+    //Can be passed to create a series of listeners.
 
     /**
      * If the start Service Intent method is called without a parameter
@@ -51,11 +67,87 @@ public class GPSPlotter {
      * @param theIntentInterval is the interval with which the AlarmManager will issue requests.
      */
     private static void issuePendingIntent(final int theIntentInterval, final Context theApplicationContext) {
+
         PendingIntent intent = PendingIntent.getService(theApplicationContext, 0, new Intent(theApplicationContext, GPSPlotterIntentService.class), 0);
         long startTime = SystemClock.elapsedRealtime();
         mAlarmManager = (AlarmManager) theApplicationContext.getSystemService(Context.ALARM_SERVICE);
         mAlarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME, startTime, theIntentInterval * TIMESTAMP_MULTIPLIER, intent);
 
+    }
+
+    /**
+     * Helper method used to initialize the location manager for "grabbing" locations
+     * using either the network or the gps of the device.
+     */
+    private static void initializeLocationManager(final Context theApplicationContext) {
+        mLocationManager = (LocationManager) theApplicationContext.getSystemService(Context.LOCATION_SERVICE);
+    }
+
+
+    /**
+     * Private class to issue a PendingIntent that will query the correct GPSPlotterListener
+     * depending upon the LocationServices that are availble.
+     *
+     * @author leachad
+     * @version 5.20.15
+     */
+    public class CoordinateIntent extends IntentService {
+
+        /**
+         * Creates an IntentService.  Invoked by your subclass's constructor.
+         *
+         * @param name Used to name the worker thread, important only for debugging.
+         */
+        public CoordinateIntent(String name) {
+            super(name);
+        }
+
+        @Override
+        protected void onHandleIntent(Intent intent) {
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, (float) 0, networkListeners[0]);
+            Log.w("INNER PRIVATE INTENT:", intent.toString());
+            Log.w("INNER PRIVATE INTENT:", networkListeners[0].mLastLocation.toString());
+
+        }
+
+    }
+
+    /**
+     * This listener allows for several variations on the same basic Location Listener interface.
+     * Calling code passes in a Static String identifying the provider.
+     *
+     * @author leachad
+     * @version 5.20.15
+     */
+    private static class GPSPlotterListener implements LocationListener {
+
+        /** Private field to hold a tag to the current Provider.*/
+        public Location mLastLocation;
+        private GPSPlotterListener(final String theProvider) {
+            Log.e(GPSPlotter.class.getName(), "LocationListener " + theProvider);
+            mLastLocation = new Location(theProvider);
+        }
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.e(GPSPlotter.class.getName(), "onLocationChanged: " + location);
+            mLastLocation.set(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Log.e(GPSPlotter.class.getName(), "onStatusChanged: " + provider + ", " + status);
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.e(GPSPlotter.class.getName(), "onProviderEnabled: " + provider);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.e(GPSPlotter.class.getName(), "onProviderDisabled: " + provider);
+        }
     }
 
 
--- app/src/main/java/location_services/GPSPlotterIntentService.java
+++ app/src/main/java/location_services/GPSPlotterIntentService.java
@@ -1,9 +1,16 @@
 package location_services;
 
 import android.app.IntentService;
-import android.content.Intent;
 import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 /**
  *
@@ -18,6 +25,9 @@ public class GPSPlotterIntentService extends IntentService {
     /** Unique String identifier for Logging purposes.*/
     private final String LOGGING_KEY = GPSPlotterIntentService.this.getClass().getName();
 
+    /** Holds a reference to the GoogleAPIClient.*/
+    private GoogleApiClient mApiClient = null;
+
 
     public GPSPlotterIntentService() {
         super("GPSPlotterIntentService");
@@ -26,10 +36,38 @@ public class GPSPlotterIntentService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         if (intent != null) {
-            Log.w(LOGGING_KEY, "Got an Intent!");
+            Log.w(LOGGING_KEY, "Got an Intent! Grabbing coordinate");
+            grabLocation();
 
         }
     }
+
+
+    /**
+     * Private helper method to grab the current location using the GooglePlayServices
+     * API.
+     *
+     */
+    private void grabLocation() {
+        LocationManager manager = (LocationManager)getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        //Drop in conditional cases here for the GPS Provider, or possibly keep a reference to the LocationManager
+        //in local Storage TODO
+        Location location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        Log.w(LOGGING_KEY, location.toString());
+
+    }
+
+    /**
+     * Private helper method to determine if the GooglePlay Services connection
+     * is valid, established and ready for use.
+     * @return googlePlayIsConnected
+     */
+    private boolean googlePlayServicesConnected() {
+        return GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext()) == ConnectionResult.SUCCESS;
+    }
+
+
+
 
 
 }
